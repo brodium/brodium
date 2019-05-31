8d1253674065c5fafe4b2b508e17419918a85f41
Jackson Tanner <jackson.tanner14@icloud.com>
2019-05-31 15:06:42 -0600

    i need a db folder from christian

--- server/controller/roomCtrl.js
+++ server/controller/roomCtrl.js
@@ -1,3 +1,8 @@
 module.exports = {
-	
+	getAllRoomsByCompany: (req, res) => {
+        const db = req.app.get('db')
+        const {co_id} = req.params
+
+        // db.getAllCompanyRooms({co_id})
+    }
 }
